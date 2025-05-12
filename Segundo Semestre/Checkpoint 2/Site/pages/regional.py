5788482c4f77442f021b0569b1e14cf08965e2ef
CavMCarolina <rm552925@fiap.com.br>
2025-05-11 21:13:23 -0300

    Finalização Regional

--- Segundo Semestre/Checkpoint 2/Site/pages/regional.py
+++ Segundo Semestre/Checkpoint 2/Site/pages/regional.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import plotly.express as px
+import pandas as pd
 from sidebar import show_sidebar
 
 st.set_page_config(page_title="Checkpoint 2", layout="wide", page_icon="images/icon2.png")
@@ -38,6 +39,15 @@ df_10 = top_10_chuvosos.sort_values(by='med_anual', ascending=False)
 
 df_10.set_index('nm', inplace=True)
 df_10 = df_10.drop('FRANCA  P11-140')
+
+st.header("Análise Regional da Precipitação")
+
+st.markdown("""
+    A análise regional da precipitação é fundamental para entender como as chuvas variam em diferentes áreas e como essas variações podem impactar a vida das pessoas e o meio ambiente. Os gráficos regionais construídos mostram a distribuição espacial das estações pluviométricas analisadas, com destaque para a média anual de precipitação de cada uma.
+    Ao utilizar a média anual como critério de cor, conseguimos perceber que determinadas regiões do país se destacam por seus elevados índices pluviométricos, o que pode estar relacionado a fatores climáticos locais, como umidade, altitude e presença de massas de ar úmidas. Essa análise espacial é essencial para entender a dinâmica da chuva no território analisado e pode auxiliar na gestão de recursos hídricos e prevenção de desastres naturais.
+    
+    O primeiro gráfico apresenta todas as estações do dataset, permitindo uma visão geral da precipitação em diferentes áreas geográficas. Já o segundo gráfico foca nas 10 estações mais chuvosas, destacando regiões com alta concentração de chuva. Essa abordagem facilita a identificação de padrões regionais, como áreas propensas a alagamentos, e também permite comparar a intensidade da precipitação entre diferentes locais.
+""")
 
 # Criando o gráfico com Plotly Express
 fig = px.scatter_mapbox(df_filtrado, lat="y", lon="x", color="med_anual", size="med_anual",
